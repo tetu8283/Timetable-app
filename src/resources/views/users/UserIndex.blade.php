4452e04cec058050c5477f190942d009ffc73253
絹張哲平 <tetu@kinubari.local>
2025-02-17 15:11:22 +0900

    create UserIndex front end

--- src/resources/views/users/UserIndex.blade.php
+++ src/resources/views/users/UserIndex.blade.php
@@ -4,57 +4,56 @@
 
 @push('styles')
     <!-- 独自の CSS ファイル -->
-    <link rel="stylesheet" href="{{ asset('css/UserIndex.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/users/index.css') }}">
 @endpush
 
 @section('content')
-    @if(@session('success'))
+    @if(session('success'))
         <div class="flash-msg">
             {{ session('success') }}
         </div>
     @endif
 
-    <table border="1px">
-        <thead>
-            <tr>
-                <th>学籍番号</th>
-                <th>名前</th>
-                <th>メールアドレス</th>
-                <th>役割</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
+    <div class="table-container">
+        <table class="centered-table" border="1px">
+            <thead>
                 <tr>
-                    <td>{{ $user->school_id }}</td>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->role }}</td>
-                    @if($user->id == Auth::id())
-                        <td>
-                            <a href="{{ route('users.edit', $user->id) }}">編集</a>
-                        </td>
-                    @endif
-
-                    @if(Auth::user()->role === 'admin')
-                        <td>
-                            <form action="{{ route('users.destroy', $user->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button
-                                    type="submit"
-                                    class="delete-button"
-                                    onclick="return confirm('削除してよろしいですか？');">
-                                    削除
-                                </button>
-                            </form>
-                        </td>
-                    @endif
+                    <th>学籍番号</th>
+                    <th>名前</th>
+                    <th>メールアドレス</th>
+                    <th>役割</th>
+                    <th></th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->school_id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->role }}</td>
+
+                        @if(Auth::user()->role === 'admin')
+                            <td>
+                                <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button
+                                        type="submit"
+                                        class="delete-button"
+                                        onclick="return confirm('削除してよろしいですか？');">
+                                        削除
+                                    </button>
+                                </form>
+                            </td>
+                        @endif
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
+
 
 @push('scripts')
 
