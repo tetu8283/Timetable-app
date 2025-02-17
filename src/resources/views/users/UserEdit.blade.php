255dc73de6d1e5520ad67ab3eb094a56748db294
絹張哲平 <tetu@kinubari.local>
2025-02-17 15:28:12 +0900

    create UserEdit front end

--- src/resources/views/users/UserEdit.blade.php
+++ src/resources/views/users/UserEdit.blade.php
@@ -3,19 +3,25 @@
 @section('title', 'プロフィール編集画面')
 
 @push('styles')
-    <!-- 独自の CSS ファイル -->
+    <link href="{{ asset('css/users/edit.css') }}" rel="stylesheet">
 @endpush
 
 @section('content')
-    <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-        <label for="name">名前</label>
-        <input type="text" name="name" value="{{ $user->name }}" required>
-        <label for="email">メールアドレス</label>
-        <input type="email" name="email" value="{{ $user->email }}" required>
-        <button type="submit">更新</button>
-    </form>
+    <div class="edit-container">
+        <form action="{{ route('users.update', $user->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <input class="input-name" type="text" name="name" value="{{ $user->name }}" placeholder="氏名" required>
+            </div>
+            <div class="form-group">
+                <input class="input-email" type="email" name="email" value="{{ $user->email }}" placeholder="メールアドレス" required>
+            </div>
+            <div class="form-group">
+                <button type="submit">更新</button>
+            </div>
+        </form>
+    </div>
 @endsection
 
 @push('scripts')
